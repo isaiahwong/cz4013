fc9851e8f31a7fb15b3eef4396308c3ddc7804c0
S-Samiksha <samikshasankar01@gmail.com>
2023-03-01 11:06:18 +0800

    update message.py

--- client/src/message.py
+++ client/src/message.py
@@ -8,7 +8,7 @@ class Err:
     def marhsall(self):
         errbytes = self.err.encode('utf-8')
         bodybytes = self.body.encode('utf-8') 
-        return struct.pack('<I', len(errbytes)) + struct.pack('<' + str(len(errbytes))+'s', errbytes) + struct.pack('<I', len(bodybytes)) + struct.pack('<' + str(len(bodybytes))+'s', bodybytes)
+        return struct.pack('<H', len(errbytes)) + struct.pack('<' + str(len(errbytes))+'s', errbytes) + struct.pack('<H', len(bodybytes)) + struct.pack('<' + str(len(bodybytes))+'s', bodybytes)
 
 
 class message:
@@ -24,10 +24,13 @@ class message:
         querybytes = ''.join([f'{key}{delimiter}{value}\n' for key, value in self.query.items()]).encode('utf-8')
         if self.error:
             errbytes = self.error.marhsall()
-            return struct.pack('<I', len(rpcbytes)) + struct.pack('<' + str(len(rpcbytes))+'s', rpcbytes) + struct.pack('<I', len(querybytes))+ struct.pack('<' + str(len(querybytes))+'s', querybytes)+self.body+errbytes
+            return struct.pack('<H', len(rpcbytes)) + struct.pack('<' + str(len(rpcbytes))+'s', rpcbytes) + struct.pack('<H', len(querybytes))+ struct.pack('<' + str(len(querybytes))+'s', querybytes)+self.body+errbytes
         else:
-            return struct.pack('<I', len(rpcbytes))+struct.pack('<' + str(len(rpcbytes))+'s', rpcbytes)+struct.pack('<I', len(querybytes))+struct.pack('<' + str(len(querybytes))+'s', querybytes)+self.body
+            return struct.pack('<H', len(rpcbytes))+struct.pack('<' + str(len(rpcbytes))+'s', rpcbytes)+struct.pack('<H', len(querybytes))+struct.pack('<' + str(len(querybytes))+'s', querybytes)+self.body
 
     
+class unmarhsall:
+    def __init__(self, msg: bytearray()):
+        self.msg = msg
 
 
