77001c4d954f25a1f26a10ef6d69831ed209e61a
isaiahwong <iisaiah.dev@gmail.com>
2023-03-01 16:06:13 +0800

    Changed sid to 16 bytes uuid

--- client/src/frame.py
+++ client/src/frame.py
@@ -3,14 +3,14 @@ from enum import Enum
 
 
 class Flag(Enum):
-    #flags for the transmission 
-    '''
+    # flags for the transmission
+    """
     SYN: Synchronization flag
-    PSH: pushing data immediately instead of waiting 
+    PSH: pushing data immediately instead of waiting
     ACK: Acknowledgement flag
     NOP: Nothing
-    FIN: Finish 
-    '''
+    FIN: Finish
+    """
     SYN = 0
     PSH = 1
     ACK = 2
@@ -21,31 +21,30 @@ class Flag(Enum):
 class Frame:
     """Represents a unit of transmission in the protocol"""
 
-    flags = set(item for item in Flag) #creates a set of all the flags 
+    flags = set(item for item in Flag)  # creates a set of all the flags
 
-    def __init__(self, flag: Flag, sid: int, data=bytearray()):
+    def __init__(self, flag: Flag, sid: bytes, data=bytearray()):
         if flag not in Frame.flags:
-            raise ValueError("Invalid flag")  
+            raise ValueError("Invalid flag")
 
         self.flag = flag.value.to_bytes(1, byteorder="little")
-        self.sid = sid.to_bytes(4, byteorder="little") #session id 
+        self.sid = sid
         self.data = data
 
         self.buffer = bytearray(self.flag)  # Encode flag
         self.buffer.extend(
             len(self.data).to_bytes(2, byteorder="little")
-            
         )  # Encode length of data
         self.buffer.extend(self.sid)
-        self.buffer.extend(self.data) 
+        self.buffer.extend(self.data)
         # [flag  len(data) sid  data]
 
 
 class Header:
 
-    size_of_flag = 1 
-    size_of_length = 2 
-    size_of_sid = 4
+    size_of_flag = 1
+    size_of_length = 2
+    size_of_sid = 16
     header_size = size_of_flag + size_of_length + size_of_sid
 
     def __init__(self, buffer: bytearray):
@@ -60,7 +59,7 @@ class Header:
         # < means little-endian H means unsigned short
         return struct.unpack("<H", self.buf[1 : 1 + 2])[0]
 
-    def streamId(self):
+    def streamId(self) -> bytes:
         # 4 bytes
         # < means little-endian I means unsigned int32
-        return struct.unpack("<I", self.buf[3:])[0]
+        return self.buf[3 : 3 + 16]
--- client/src/protocol.py
+++ client/src/protocol.py
@@ -2,18 +2,19 @@ import socket
 import time
 import select
 from frame import Header, Frame, Flag
+import uuid
 
 
 class Stream:
     def __init__(self, sess, sid, maxFrameSize, deadline=5):
-        self.session = sess #the session
-        self.sid = sid #session id
-        self.maxFrameSize = maxFrameSize #max frame size in the session 
-        self.deadline = deadline 
+        self.session = sess  # the session
+        self.sid = sid  # session id
+        self.maxFrameSize = maxFrameSize  # max frame size in the session
+        self.deadline = deadline
 
     def write(self, data=bytearray()):
-        frame = Frame(Flag.PSH, self.sid, data) #create the frame 
-        bts = data 
+        frame = Frame(Flag.PSH, self.sid, data)  # create the frame
+        bts = data
         while len(bts) > 0:
             size = len(data)
             if size > self.maxFrameSize:
@@ -30,7 +31,8 @@ class Stream:
         while True:
             ready = select.select([self.session.sock], [], [], self.deadline)
             if not ready[0]:
-                raise TimeoutError("Read timed out")
+                raise TimeoutError("Read timeout")
+
             d, addr = self.session.sock.recvfrom(1024)
             buffer = bytearray(d)
             header = Header(buffer)
@@ -54,19 +56,20 @@ class Stream:
 
 class Session:
     def __init__(self, sock: socket.socket, target: tuple):
-        self.sid = 0 
         self.sock = sock
         self.target = target
         self.streams = {}
 
     def open(self):
-        self.sid += 2 
-        self.writeFrame(frame=Frame(Flag.SYN, self.sid)) #calling the writeframe function to send synchronization 
-        self.streams[self.sid] = Stream(self, self.sid, 1024) #calling stream class 
-        return self.streams[self.sid]
+        sid = uid.uuid4().bytes[:16]
+        # calling the writeframe function to send synchronization
+        self.writeFrame(frame=Frame(Flag.SYN, bytes(sid)))
+        stream = Stream(self, bytes(sid), 1024)
+        self.streams[str(sid)] = stream
+        return stream
 
     def writeFrame(self, frame: Frame, deadline: int = 0):
-        self.sock.sendto(frame.buffer, self.target) #why not sendall why sendto?
+        self.sock.sendto(frame.buffer, self.target)  # why not sendall why sendto?
 
 
 class Client:
@@ -74,8 +77,12 @@ class Client:
         self.addr = addr
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #to reuse ip address cuz of the timeout issue but can we do this for udp?
-        self.session = Session(sock=self.sock, target=(addr, port)) #call the session class 
+        self.sock.setsockopt(
+            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
+        )  # to reuse ip address cuz of the timeout issue but can we do this for udp?
+        self.session = Session(
+            sock=self.sock, target=(addr, port)
+        )  # call the session class
 
     def open(self):
         return self.session.open()
